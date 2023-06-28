e0f933e8c677dfdbcebc90071f72ef265154e8ec
John Van Ham <john@vanh.am>
2023-06-28 20:50:55 +0100

    Add index route and catchall route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "URL Shortener"
+}
+
+#[get("/<url>")]
+fn shorten(url: String) -> String {
+    format!("Shortened URL: {}", url)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![shorten])
 }
