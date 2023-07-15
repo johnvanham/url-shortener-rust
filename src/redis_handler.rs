af2672b8991f84cc7a9cc8746a0a08c57e78a54c
John Van Ham <john@vanh.am>
2023-07-15 21:58:50 +0100

    Load redis connection URL from REDIS_URL env var

--- src/redis_handler.rs
+++ src/redis_handler.rs
@@ -2,7 +2,7 @@ extern crate redis;
 use redis::Commands;
 
 pub fn get_url_from_redis(key: String) -> redis::RedisResult<String> {
-    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let client = redis::Client::open(get_redis_url())?;
     let mut con = client.get_connection()?;
 
     let url: String = con.get(key)?;
@@ -11,10 +11,21 @@ pub fn get_url_from_redis(key: String) -> redis::RedisResult<String> {
 }
 
 pub fn set_url_in_redis(key: String, url: String) -> redis::RedisResult<String> {
-    let client = redis::Client::open("redis://127.0.0.1")?;
+    let client = redis::Client::open(get_redis_url())?;
     let mut con = client.get_connection()?;
 
     let result = con.set(key, url)?;
 
     Ok(result)
+}
+
+// Function to get REDIS_URL from environment variable
+pub fn get_redis_url() -> String {
+    let redis_url_var = std::env::var("REDIS_URL");
+    let redis_url = match redis_url_var {
+        Ok(url) => Some(url),
+        Err(_) => None
+    };
+
+    redis_url.unwrap().to_string()
 }
